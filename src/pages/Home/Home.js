10df66303c31ae7734d3206dceb2c742e9d8b982
LeVietDuc2000 <levietduc2000cb@gmail.com>
2022-11-21 15:44:10 +0700

    them spinner

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { MdDevicesOther } from 'react-icons/md';
+import { FaUsers } from 'react-icons/fa';
 
 const Home = () => {
   return (
-    <h1 className="text-3xl font-bold underline">Đây là trang Home Page</h1>
+    <div>
+      <div className="grid grid-cols-6 gap-4 p-4 bg-[#EEEEEE]">
+        <div className="bg-white h-20 rounded-md border-2 border-solid boder-[#305680]">
+          <div className="text-sm font-bold text-center leading-7">
+            Total Devices
+          </div>
+          <div className="text-2xl font-bold flex justify-center items-center leading-8">
+            <span className="w-10 h-8 flex justify-center items-center">
+              <MdDevicesOther />
+            </span>
+            <span>20</span>
+          </div>
+        </div>
+        <div className="bg-white h-20 rounded-md border-2 border-solid boder-[#305680]">
+          <div className="text-sm font-bold text-center leading-7">
+            Total Users
+          </div>
+          <div className="text-2xl font-bold flex justify-center items-center leading-8">
+            <span className="w-10 h-8 flex justify-center items-center">
+              <FaUsers />
+            </span>
+            <span>20</span>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[#EEEEEE] h-screen">
+        <div className="h-full w-full bg-white mx-4 mb-4">
+          Giới thiệu các tính năng của IoT Platform
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,7 +1,89 @@
 import React from 'react';
+import testAvatar from '~/assets/image/background_iot_platform.JPG';
 
 const Profile = () => {
-  return <div>Đây là trang profile</div>;
+  return (
+    <div>
+      <div className="border-b-2 block md:flex">
+        <div className="w-full md:w-2/5 p-4 sm:p-6 lg:p-8 bg-white shadow-md">
+          <div className="flex justify-between">
+            <span className="text-xl font-semibold block">Duc</span>
+            <a
+              href="/"
+              className="-mt-2 text-md font-bold text-white bg-gray-700 rounded-full px-5 py-2 hover:bg-gray-800"
+            >
+              Save
+            </a>
+          </div>
+
+          <div className="text-gray-600 text-center">Administration</div>
+          <div className="w-full p-8 flex justify-center">
+            <label htmlFor="avatar_user" className="cursor-pointer">
+              <img
+                className="p-1 w-28 h-28 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
+                src={testAvatar}
+                alt="Bordered avatar"
+              />
+            </label>
+            <input type="file" id="avatar_user" hidden></input>
+          </div>
+        </div>
+
+        <div className="w-full md:w-3/5 p-8 bg-white lg:ml-4 shadow-md">
+          <div className="rounded  shadow p-6">
+            <div className="pb-6">
+              <label
+                htmlFor="firstName"
+                className="font-semibold text-gray-700 block pb-1 text-left"
+              >
+                First Name
+              </label>
+              <div className="flex">
+                <input
+                  id="firstName"
+                  name="firstName"
+                  className="border-1  rounded-r px-4 py-2 w-full"
+                  type="text"
+                  defaultValue="Lê Việt"
+                />
+              </div>
+            </div>
+            <div className="pb-6">
+              <label
+                htmlFor="lastName"
+                className="font-semibold text-gray-700 block pb-1 text-left"
+              >
+                Last Name
+              </label>
+              <div className="flex">
+                <input
+                  id="lastName"
+                  name="lastName"
+                  className="border-1  rounded-r px-4 py-2 w-full"
+                  type="text"
+                  defaultValue="Đức"
+                />
+              </div>
+            </div>
+            <div className="pb-4">
+              <label
+                htmlFor="about"
+                className="font-semibold text-gray-700 block pb-1 text-left"
+              >
+                Email
+              </label>
+              <input
+                id="email"
+                className="border-1  rounded-r px-4 py-2 w-full"
+                type="email"
+                defaultValue="duc2000cb@gmail.com"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
